c2e305ad0cb0900ee9b5bda6a62432ee5013c778
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-07-10 01:18:32 +0200

    Resolv error parameters not found

--- Services/CustomService.php
+++ Services/CustomService.php
@@ -4,14 +4,20 @@ namespace Custom\StandaloneBundle\Services;
 
 class CustomService
 {
+	private $number;
+
+	public function __construct($number) {
+		$this->number = $number;
+	}
+
 	/**
 	* Check if the number is even or noy
 	*
 	* @param int $number
 	* @return bool
 	*/
-	public function isEven($number)
+	public function isEven()
 	{
-		return $number % 2;
+		return $this->number % 2;
 	}
 }
